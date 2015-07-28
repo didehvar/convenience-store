d9cd385134174cf946605ef2d7f21ecb959a4290
James Didehvar <james@didehvar.com>
2015-07-28 23:50:47 +0100

    Remove redecleration

--- spec/rule/bulk_spec.rb
+++ spec/rule/bulk_spec.rb
@@ -17,9 +17,6 @@ RSpec.describe Bulk do
     end
 
     it 'works out cost differences based on the defined rule' do
-      @item_a = Item.new Item.products.keys.first.to_s
-      @item_b = Item.new Item.products.keys.at(1).to_s
-
       new_price = @item_a.price - 1
 
       rule = Bulk.new(@item_a.code, 3, new_price)
