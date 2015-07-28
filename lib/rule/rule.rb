41e37ecb09ceb40848faf1cf4dda0bdff8790a8f
James Didehvar <james@didehvar.com>
2015-07-28 22:47:31 +0100

    Return zero instead of nil for default rule

--- lib/rule/rule.rb
+++ lib/rule/rule.rb
@@ -1,6 +1,11 @@
 class Rule
   attr_accessor :items
 
+  def calculate(items)
+    @items = items
+    return 0
+  end
+
   def count(code)
     @items.select { |i| i.code == code }.size
   end
--- spec/rule/rule_spec.rb
+++ spec/rule/rule_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe Rule do
+  before :all do
+    @item_a = Item.new Item.products.keys.first.to_s
+    @item_b = Item.new Item.products.keys.at(1).to_s
+    @items = [@item_a, @item_a, @item_a, @item_b]
+  end
+
+  describe '#calculate' do
+    it 'returns zero as no rule has been defined' do
+      rule = Rule.new
+      expect(rule.calculate(@items)).to eq 0
+    end
+  end
+
   describe '#count' do
     it 'counts the occurrences of an item code' do
-      a = Item.new Item.products.keys.first.to_s
-      b = Item.new Item.products.keys.at(1).to_s
-
       rule = Rule.new
-      rule.items = [a, a, a, b]
+      rule.items = @items
 
-      expect(rule.count(a.code)).to eq 3
-      expect(rule.count(b.code)).to eq 1
+      expect(rule.count(@item_a.code)).to eq 3
+      expect(rule.count(@item_b.code)).to eq 1
     end
   end
 end
