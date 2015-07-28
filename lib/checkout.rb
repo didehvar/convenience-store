fbca2d1320e29d10dcfd6b0be93fea7fa594ad5b
James Didehvar <james@didehvar.com>
2015-07-28 23:18:24 +0100

    Add checkout support and tests

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,8 +1,10 @@
+require 'pp'
+
 class Checkout
   attr_reader :rules
 
   def initialize(pricing_rules)
-    @rules = pricing_rules
+    @rules = Array(pricing_rules)
     @items = []
   end
 
@@ -11,6 +13,8 @@ class Checkout
   end
 
   def total
-
+    sum = @items.inject(0) { |sum, item| sum + item.price }
+    @rules.each { |r| sum += r.calculate(@items) }
+    sum
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,14 +1,42 @@
 require 'spec_helper'
 
 RSpec.describe Checkout do
+  before :all do
+    @item_a = Item.new Item.products.keys.first.to_s
+    @item_b = Item.new Item.products.keys.at(1).to_s
+  end
+
   describe '#scan' do
     it 'adds an item to the checkout basket' do
       co = Checkout.new Rule.new
 
-      item = Item.new Item.products.keys.first.to_s
-      co.scan(item)
+      @item_a = Item.new Item.products.keys.first.to_s
+      co.scan(@item_a)
 
-      expect(co.instance_variable_get(:@items)).to include item
+      expect(co.instance_variable_get(:@items)).to include @item_a
+    end
+  end
+
+  describe '#total' do
+    it 'totals the checkout basket' do
+      co = Checkout.new Rule.new
+
+      co.scan(@item_a)
+      2.times { co.scan @item_b }
+
+      expect(co.total).to eq @item_a.price + @item_b.price * 2
+    end
+
+    it 'totals the checkout basket, including any rules' do
+      co = Checkout.new Bulk.new(@item_a.code, 3, @item_a.price - 2)
+
+      4.times { co.scan @item_a }
+      3.times { co.scan @item_b }
+
+      # calcaulate cost, take off discount
+      # (-2 per each @item_a when 3+ @item_a in basket)
+      total = (@item_a.price * 4) + (@item_b.price * 3) - (2 * 4)
+      expect(co.total).to eq total
     end
   end
 end
