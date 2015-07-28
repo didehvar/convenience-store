4adc16236ef1799d0be952176ec294ed08d4e17e
James Didehvar <james@didehvar.com>
2015-07-28 17:00:50 +0100

    Create initial project structure

---
+++ lib/convenience/store.rb
@@ -0,0 +1,7 @@
+require "convenience/store/version"
+
+module Convenience
+  module Store
+    # Your code goes here...
+  end
+end
---
+++ lib/convenience/store/version.rb
@@ -0,0 +1,5 @@
+module Convenience
+  module Store
+    VERSION = "0.1.0"
+  end
+end
